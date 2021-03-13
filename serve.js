779fc0e0ee4a982a9bc930f7c1979789943284ee
Paulo Sobrinho Ferreira <paulojhole@gmail.com>
2021-03-13 17:49:47 -0300

    corrigindo erro de escrita

--- serve.js
+++ serve.js
@@ -1,4 +1,4 @@
 //arquivo somente para inicializar o projeto
 const app = require('./src');
 
-app.listen(8080, () => console.info('rodando na portar 8080'));
+app.listen(8080, () => console.info('rodando na porta 8080'));
